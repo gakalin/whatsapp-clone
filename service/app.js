5df35cbe925aacce1345b856ab464ce08377c9c6
gakalin <gokberkakalin@gmail.com>
2021-03-22 23:25:57 +0300

    added: CORS settings

--- service/app.js
+++ service/app.js
@@ -2,9 +2,11 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const db = require('./db/connection')();
+const cors = require('cors');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors({ origin: process.env.CORS_URL, credentials: true }));
 
 require('./routes')(app);
 app.listen(process.env.PORT, () => {
