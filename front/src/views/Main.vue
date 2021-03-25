cc33d754c50c391608c4b52744b7e24190fedfe1
gakalin <gokberkakalin@gmail.com>
2021-03-25 20:51:59 +0300

    fixed: some css errors on main view

--- front/src/views/Main.vue
+++ front/src/views/Main.vue
@@ -1,6 +1,6 @@
 <template>
   <v-card flat class="d-flex">
-    <v-card flat height="100vh" width="410px" color="white" class="rounded-0 row"> 
+    <v-card flat height="100vh" width="410px" color="white" class="rounded-0"> 
       
       <v-card flat width="100%" height="60" color="#EDEDED" class="px-4 py-3 d-flex justify-space-between rounded-0">
         <v-avatar size="38" class="rounded-circle">
@@ -19,7 +19,7 @@
       </v-card>
 
       <v-card flat class="rounded-0 msgPanel overflow-y-auto">
-        <v-card color="grey lighten-3" flat class="rounded-0 pa-3 d-flex row" style="border-bottom: 1px solid #e2e0e0;">
+        <v-card color="grey lighten-3" flat class="rounded-0 pa-3 d-flex" style="border-bottom: 1px solid #e2e0e0;">
           <v-avatar size="47" class="rounded-circle">
             <img src="../assets/blank_avatar.jpg">
           </v-avatar>
@@ -30,14 +30,14 @@
               <small>19:07</small>
             </div>
 
-            <div class="d-flex row">
+            <div class="d-flex">
               <v-icon color="blue">mdi-check-all</v-icon>
               <span class="pl-1 subtitle-2 align-self-center d-inline-block text-truncate font-weight-light"> Yinelenen bir sayfa içeriğinin okuyucunun dikkatini dağıttığı bilinen bir gerçektir.</span>
             </div>
           </div>
         </v-card>
 
-        <v-card flat class="rounded-0 pa-3 d-flex row" style="border-bottom: 1px solid #e2e0e0;">
+        <v-card flat class="rounded-0 pa-3 d-flex" style="border-bottom: 1px solid #e2e0e0;">
           <v-avatar size="47" class="rounded-circle">
             <img src="../assets/blank_avatar.jpg">
           </v-avatar>
@@ -48,7 +48,7 @@
               <small>19:07</small>
             </div>
 
-            <div class="d-flex row">
+            <div class="d-flex">
               <v-icon color="grey">mdi-check-all</v-icon>
               <span class="pl-1 subtitle-2 align-self-center d-inline-block text-truncate font-weight-light">message</span>
             </div>
@@ -58,14 +58,14 @@
 
     </v-card>
 
-    <v-card flat height="100vh" width="100%" class="msgSide rounded-0 row">
+    <v-card flat height="100vh" width="100%" class="msgSide rounded-0">
 
       <v-card flat width="100%" height="60" color="#EDEDED" class="px-4 py-3 d-flex justify-space-between rounded-0" style="border-bottom: 1px solid #d3d3d3; border-left: 1px solid #d3d3d3;">
         <div class="d-flex">
-          <v-avatar size="38" class="rounded-circle row">
+          <v-avatar size="38" class="rounded-circle">
             <img src="../assets/blank_avatar.jpg">
           </v-avatar>
-          <h4 class="row align-self-center pl-3">Gökberk Akalın</h4>
+          <h4 class="align-self-center pl-3">Gökberk Akalın</h4>
         </div>
         <div>
           <v-btn icon class="mr-4"><v-icon>mdi-magnify</v-icon></v-btn>
@@ -73,7 +73,7 @@
         </div>
       </v-card>
 
-      <v-card class="msgMain overflow-y-auto" flat color="transparent">
+      <v-card class="msgMain overflow-y-auto flex" flat color="transparent">
         <v-card class="pa-2 d-flex justify-start" color="transparent" flat>
           <div>
             <span>Yinelenen bir sayfa içeriğinin okuyucunun dikkatini dağıttığı bilinen bir gerçektir. Lorem Ipsum kullanmanın amacı, sürekli 'buraya metin gelecek, buraya metin gelecek' yazmaya kıyasla daha dengeli bir harf dağılımı sağlayarak okunurluğu artırmasıdır. Şu anda birçok masaüstü yayıncılık paketi ve web sayfa düzenleyicisi, varsayılan mıgır metinler olarak Lorem Ipsum kullanmaktadır. Ayrıca arama motorlarında 'lorem ipsum' anahtar sözcükleri ile arama yapıldığında henüz tasarım aşamasında olan çok sayıda site listelenir. Yıllar içinde, bazen kazara, bazen bilinçli olarak (örneğin mizah katılarak), çeşitli sürümleri geliştirilmiştir.
@@ -87,10 +87,16 @@
             <span class="date">22:00</span>
           </div>
         </v-card>
+        <v-card right class="me pa-2 d-flex justify-end" color="transparent" flat>
+          <div>
+            <span>avadfasd fasdf456as d4f65asd4f65asd4f6</span>
+            <span class="date">22:00</span>
+          </div>
+        </v-card>
       </v-card>
 
       <v-footer color="#EDEDED" padless absolute fixed width="100%" class="rounded-0 ma-0 pa-0" style="border-top: 1px solid #d3d3d3;">
-        <v-text-field clearable dense flat solo hide-details class="pa-3 row rounded-xl ma-0 pa-0" label="Type a message"></v-text-field>
+        <v-text-field clearable dense flat solo hide-details class="pa-3 rounded-xl ma-0 pa-0" label="Type a message"></v-text-field>
       </v-footer>
 
     </v-card>
@@ -98,17 +104,8 @@
   </v-card>
 </template>
 
-<script>
-export default {
-  beforeMount() {
-    if (!this.$store.state.userId) {
-      this.$router.push({ name: 'Login' }).catch(() => {});
-    }
-  }
-}
-</script>
 
-<style>
+<style scoped>
 .msgMain .v-card div span.date {
   font-size: 0.75rem;
   color: #9b9b9b;
@@ -152,3 +149,13 @@ export default {
   max-height: calc(100vh - 110px);
 }
 </style>
+
+<script>
+export default {
+  beforeMount() {
+    /*if (!this.$store.state.userId) {
+      this.$router.push({ name: 'Login' }).catch(() => {});
+    }*/
+  }
+}
+</script>
